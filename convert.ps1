c5a13a52d50191179865e35c36e8147ce46807f9
Blake Haug <33404760+BNH440@users.noreply.github.com>
2021-09-02 03:11:28 +0000

    added number check

--- convert.ps1
+++ convert.ps1
@@ -2,4 +2,9 @@ $gifRepeat = Read-Host -Prompt "Amount of times to repeat the gif (default: 1)"
 
 $gifSpeed = Read-Host -Prompt "Video speed (higher is slower) (default: 1)"
 
+if (-NOT ([Microsoft.VisualBasic.Information]::IsNumeric($gifRepeat)) -OR -NOT ([Microsoft.VisualBasic.Information]::IsNumeric($gifSpeed))) {
+    Write-Error "Non-numeric values"
+    exit
+}
+
 ./ffmpeg/bin/ffmpeg.exe -stream_loop $gifRepeat -i file.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -filter:v "setpts=$gifSpeed*PTS" file.avi
